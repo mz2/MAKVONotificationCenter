d05373baa2f69496c1b774d585d6a835de37cb56
mz2 <matias.piipari@gmail.com>
2014-11-20 19:34:16 -0400

    Adds an OSX framework target.

---
+++ MAKVONotificationCenter/MAKVONotificationCenter-Framework.h
@@ -0,0 +1,20 @@
+//
+//  MAKVONotificationCenter.h
+//  MAKVONotificationCenter
+//
+//  Created by Matias Piipari on 20/11/2014.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MAKVONotificationCenter.
+FOUNDATION_EXPORT double MAKVONotificationCenterVersionNumber;
+
+//! Project version string for MAKVONotificationCenter.
+FOUNDATION_EXPORT const unsigned char MAKVONotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAKVONotificationCenter/PublicHeader.h>
+
+#import "MAKVONotificationCenter.h"
+
